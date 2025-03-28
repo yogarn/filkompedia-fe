8fbc75478a134426ee865d95be070662ab106b54
yogarn <radityanalaa@gmail.com>
2025-03-28 15:32:34 +0700

    `chore:` Add props to check role

---
+++ src/props/requiredRole.tsx
@@ -0,0 +1,50 @@
+import { useAuthFetch } from "@/hooks/useAuthFetch";
+import { useEffect, useState } from "react";
+
+interface User {
+    id: string;
+    username: string;
+    email: string;
+    roleId: number;
+}
+
+interface RequireRoleProps {
+    allowedRole: number;
+    children: React.ReactNode;
+}
+
+const RequireRole = ({ allowedRole, children }: RequireRoleProps) => {
+    const { fetchWithAuth } = useAuthFetch();
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchWithAuth(`${import.meta.env.VITE_API_URL}/users/me`)
+            .then(res => res.json())
+            .then(data => {
+                setCurrentUser(data.data);
+            })
+            .catch(err => console.error("Error fetching user:", err))
+            .finally(() => setLoading(false));
+    }, [fetchWithAuth]);
+
+    if (loading) {
+        return <p className="text-center"></p>;
+    }
+
+    if (!currentUser || currentUser.roleId !== allowedRole) {
+        return (
+            <div className="flex flex-col items-center p-6 space-y-4 w-full">
+                <div className="flex justify-center items-center w-full h-full">
+                    <p className="text-gray-700 text-center">
+                        You don't belong here! Try searching for something else!
+                    </p>
+                </div>
+            </div>
+        );
+    }
+
+    return <>{children}</>;
+};
+
+export default RequireRole;
